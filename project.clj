327436ace78161bb06e30741940510fdb3857f3b
jarppe <jarppe@metosin.fi>
2014-02-09 23:32:16 +0200

    can't make ring-json working with empty body

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jarppe.btest "0.0.2-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[ring "1.2.1"]
-                                  [ring/ring-json "0.2.0"]
+                                  [cheshire "5.3.0"]
                                   [compojure "1.1.6"]]
                    :jvm-opts ["-Xverify:none"]}})
