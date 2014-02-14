0e4fbd0aedcc773ae66e4e7f659dfacf34dc3ac6
jarppe <jarppe@metosin.fi>
2014-02-14 10:28:02 +0200

    0.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarppe.btest "0.0.5"
+(defproject jarppe.btest "0.0.6-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [slingshot "0.10.3"]]
   :profiles {:dev {:dependencies [[ring "1.2.1"]
